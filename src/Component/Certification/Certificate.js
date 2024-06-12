e27e524de796b62b695caf36c385c1af82305b49
Hemanthfitsol <146804316+Hemanthfitsol@users.noreply.github.com>
2024-06-12 16:43:06 +0530

    added changes for buttons

--- src/Component/Certification/Certificate.js
+++ src/Component/Certification/Certificate.js
@@ -1,63 +1,70 @@
 import React from "react";
-import logo from '.././SpinMain/LogoForm.png';
+import logo from '../SpinMain/LogoForm.png';
 import qrcode from "./qrcode.png";
-
-const Certificate = ({ company, amount, date, retirementId, standard }) => {
+import sign from '../SpinMain/Sign.png';
+const Certificate = ({ company, amount, retirementId, standard }) => {
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
   return (
     <div className="m-5 mr-5 ml-5 border-4 border-green-600 mx-auto p-5 rounded-lg bg-white shadow-md mt-5 w-26.25 h-37.188">
       <div className="text-center mb-5">
-        <div className="flex justify-between items-center mx-5">
+        <div className="flex justify-between items-center mx-[12%]">
           <div className="text-left">
             <h1 className="text-2xl mb-2 font-bold">
               2024
               <br />
               Verified Carbon Offset Certificate
             </h1>
-            <hr className="border-t-4 border-D7E5BE w-full" />
+            <hr className="border-t-4 border-D7E5BE w-[118%] ml-[-5%]" />
           </div>
-          <img src={logo} alt="" className="h-16 ml-5" />
+          <img src={logo} alt="" className="h-25 mx-[-5%]" />
         </div>
       </div>
-      <div className="text-left mb-5">
-        <p className="ml-5">PRESENTED TO</p>
-        <h2 className="text-xl font-semibold ml-5 text-green-600 mb-5">
+      <div className="text-left mb-5 mx-[10%]">
+        <p className="ml-5 ">PRESENTED TO</p>
+        <h2 className="text-2xl font-bold ml-5 text-green-600 mb-5">
           {company}
         </h2>
         <p className="ml-5">FOR OFFSETTING</p>
-        <h2 className="text-xl font-semibold ml-5 text-green-600">
-          {amount} tCO2e
+        <h2 className="text-2xl font-bold ml-5 text-green-600">
+          {amount} tCO<sub>2</sub>e
         </h2>
       </div>
-      <div className="text-left mb-5 ml-5">
+      <div className="text-left ml-[6%] mb-5 mr-[5%] text-lg">
         <p>
-          Climate change and protecting the environment go hand in hand. Your
-          continued investment in your customers, your community, and the
-          environment will have a measurable impact for years to come.
+          Climate change and environmental protection are closely linked. Your
+          efforts to enhance the environment will create a significant and
+          long-term impact.
           <br />
         </p>
-        <p className="mt-5">
-          Fitsol thanks you for committing to bold climate action and
-          contributing to the Sustainable Development Goals set out by the UN.
+        <p className="mt-3">
+          Fitsol deeply appreciates your dedication to taking decisive climate
+          action and for supporting the UN's Sustainable Development Goals.
         </p>
       </div>
-      <div className="text-left mb-5">
-        <div className="flex justify-end">
-          <div className="mr-5 ">
+      <div className="text-left mb-5 ml-[5%]">
+        <div className="flex justify-end mr-[5%]">
+          <div className=" mr-5">
             Date:
-            <br /> {date}
+            <br /> {getCurrentDate()}
           </div>
           <div className="mr-5 ml-5 ">
-            CEO Fitsol
+            CEO Fitsol Signature
             <br />
-            Signature
+            <img src={sign} alt="" className="ml-4 h-25 w-20" />
+            <br />
           </div>
-
           <div className="text-center mr-5 ml-5">
             QR Code <br />
             <img src={qrcode} alt="QR Code" className="w-20 h-20" />
           </div>
         </div>
-        <div className="mx-5">
+        <div className="mx-5 mb-0 ">
           <p>
             <b>Retirement ID</b>: {retirementId}
           </p>
@@ -69,32 +76,32 @@ const Certificate = ({ company, amount, date, retirementId, standard }) => {
           </p>
         </div>
       </div>
-      <div className="text-left mt-0 mb-5 mx-5">
-        <p className="mb-5">
-          Fitsol commits to retire the purchased carbon credits or certificates
-          in the relevant public registry in line with the highest International
-          carbon accounting and reporting standards. These credits are utilized
-          to offset estimated carbon footprint of beneficiary for year 2023-24
+      <div className="text-left mb-5 mx-5 text-md mx-[5%]">
+        <p className="mb-3 ml-5 mt-0">
+          These credits are utilized to offset the estimated carbon footprint of
+          the beneficiary for year 2023-24
         </p>
         <hr className="border-t-4 border-D7E5BE w-full" />
         <center>
-          <p>
+          <p className="text-sm">
             This certificate is issued by Fitsol Supply Chain Solutions Private
             Limited. For more information about our services and climate change
-            mitigation projects, please{" "}
+            mitigation projects, please visit:{" "}
             <a href="http://www.Fitsol.green" className="text-blue-500">
               www.Fitsol.green
             </a>{" "}
-            <p>
-              The CeO2 emissions indicated on the certificate are compensated
-              through investments in carbon offset projects based on
+            The CeO<sub>2</sub> emissions indicated on the certificate are
+            compensated through investments in carbon offset projects based on
+            international standards.
+            {/* <p>
+              The CeO <sub>2</sub>emissions indicated on the certificate are
+              compensated through investments in carbon offset projects based on
               international standards.
-            </p>
+            </p> */}
           </p>
         </center>
       </div>
     </div>
   );
 };
-
 export default Certificate;
--- src/Component/SpinMain/SpinWheel/Thanks.js
+++ src/Component/SpinMain/SpinWheel/Thanks.js
@@ -1,14 +1,30 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const ThankYou = () => {
+  const navigate = useNavigate();
+  const handleClose = () => {
+    navigate("/");
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="bg-white rounded-lg shadow-lg p-8 max-w-lg text-center">
-        <h1 className="text-3xl font-bold text-green-600 mb-4">🎉 Thank You! 🎉</h1>
+        <h1 className="text-3xl font-bold text-green-600 mb-4">
+          🎉 Thank You! 🎉
+        </h1>
         <p className="text-lg text-gray-700 mb-6">
-          Thank you for participating in our contest! Your support for sustainable practices and environmental conservation is greatly appreciated. 🌱🌍
+          Thank you for participating in our contest! Your support for
+          sustainable practices and environmental conservation is greatly
+          appreciated. 🌱🌍
         </p>
-        <p className="text-lg text-green-800 font-bold">We hope to see you again in future events!</p>
+        <p className="text-lg text-green-800 font-bold">
+          We hope to see you again in future events!
+        </p>
+        <button
+          onClick={handleClose}
+          className="px-4 py-2 my-4 bg-buttonColor text-black font-bold rounded hover:bg-hoveColor transition duration-300"
+        >
+          return to home page
+        </button>
       </div>
     </div>
   );
